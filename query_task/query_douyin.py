64c7dce259d47f3c3ca339ee5ce91a6ab26d777e
nfe-w <nfe-w@outlook.com>
2024-05-28 18:42:56 +0800

    feat(抖音-直播): 支持纯数字抖音号的开播提醒 #4

--- query_task/query_douyin.py
+++ query_task/query_douyin.py
@@ -5,8 +5,10 @@ from urllib import parse
 
 import requests
 from bs4 import BeautifulSoup
+from requests.utils import dict_from_cookiejar
 
 from common import util
+from common.cache import set_cached_value, get_cached_value
 from common.logger import log
 from common.proxy import my_proxy
 from query_task import QueryTask
@@ -19,11 +21,13 @@ class QueryDouyin(QueryTask):
         self.username_list = config.get("username_list", [])
         self.sec_uid_list = config.get("sec_uid_list", [])
         self.douyin_id_list = config.get("douyin_id_list", [])
+        self.ttwid = None
 
     def query(self):
         if not self.enable:
             return
         try:
+            self.init_ttwid()
             current_time = time.strftime("%H:%M", time.localtime(time.time()))
             if self.begin_time <= current_time <= self.end_time:
                 my_proxy.current_proxy_ip = my_proxy.get_proxy(proxy_check_url="https://www.iesdouyin.com/web/api/v2/aweme/post")
@@ -32,7 +36,7 @@ class QueryDouyin(QueryTask):
                         self.query_dynamic(self.username_list[i], self.sec_uid_list[i])
                 if self.enable_living_check:
                     for douyin_id in self.douyin_id_list:
-                        self.query_live_status_v2(douyin_id)
+                        self.query_live_status_v3(douyin_id)
         except Exception as e:
             log.error(f"【抖音-查询任务-{self.name}】出错：{e}", exc_info=True)
 
@@ -43,6 +47,41 @@ class QueryDouyin(QueryTask):
         except Exception:
             log.error("【抖音-获取签名】连接失败")
             return ""
+
+    def init_ttwid(self, get_from_cache=True):
+        ttwid = None
+        if get_from_cache:
+            ttwid = get_cached_value("ttwid")
+        if ttwid is None:
+            ttwid = self.generate_ttwid()
+            set_cached_value("ttwid", ttwid)
+        self.ttwid = ttwid
+
+    def generate_ttwid(self):
+        url = "https://ttwid.bytedance.com/ttwid/union/register/"
+        headers = self.get_headers()
+        # 固定的内容
+        body_data = {
+            "region": "cn",
+            "aid": 1768,
+            "needFid": False,
+            "service": "www.ixigua.com",
+            "migrate_info": {
+                "ticket": "",
+                "source": "node"
+            },
+            "cbUrlProtocol": "https",
+            "union": True
+        }
+        response = util.requests_post(url, f"抖音-生成ttwid-{self.name}", headers=headers, json=body_data)
+        if util.check_response_is_ok(response):
+            cookie_dict = dict_from_cookiejar(response.cookies)
+            ttwid = cookie_dict.get('ttwid')
+            log.info(f"【抖音-生成ttwid-{self.name}】成功：{ttwid}")
+            return ttwid
+        else:
+            log.error(f"【抖音-生成ttwid-{self.name}】请求返回数据code错误：{response.status_code}")
+            return None
 
     def query_dynamic(self, nickname=None, sec_uid=None):
         if nickname is None or sec_uid is None:
@@ -88,12 +127,13 @@ class QueryDouyin(QueryTask):
                         log.error(f"【抖音-查询动态状态-{self.name}】dict取值错误，nickname：{nickname}")
                         return
 
+    @DeprecationWarning
     def query_live_status_v2(self, user_account=None):
         if user_account is None:
             return
         query_url = f"https://live.douyin.com/{user_account}?my_ts={int(time.time())}"
         headers = self.get_headers_for_live()
-        response = util.requests_get(query_url, "抖音-查询直播状态", headers=headers, use_proxy=True)
+        response = util.requests_get(query_url, f"抖音-查询直播状态-{self.name}", headers=headers, use_proxy=True)
         if util.check_response_is_ok(response):
             html_text = response.text
             soup = BeautifulSoup(html_text, "html.parser")
@@ -150,6 +190,63 @@ class QueryDouyin(QueryTask):
                             log.info(f"【抖音-查询直播状态-{self.name}】【{nickname}】开播了，准备推送：{room_title}")
                             self.push_for_douyin_live(name, query_url, room_title, room_cover_url)
 
+    def query_live_status_v3(self, user_account=None):
+        if user_account is None:
+            return
+        query_url = "https://live.douyin.com/webcast/room/web/enter/"
+        headers = self.get_headers_for_live()
+        if self.ttwid is not None:
+            headers['cookie'] = f"ttwid={self.ttwid}"
+        params = {
+            "aid": "6383",
+            "device_platform": "web",
+            "enter_from": "web_live",
+            "cookie_enabled": "true",
+            "browser_language": "zh-CN",
+            "browser_platform": "Win32",
+            "browser_name": "Chrome",
+            "browser_version": "109.0.0.0",
+            "web_rid": f"{user_account}",
+        }
+        response = util.requests_get(query_url, f"抖音-查询直播状态-{self.name}", headers=headers, params=params, use_proxy=True)
+        if util.check_response_is_ok(response):
+            result = json.loads(str(response.content, "utf-8"))
+            if result["status_code"] != 0:
+                log.error(f"【抖音-查询直播状态-{self.name}】请求返回数据code错误：{result['status_code']}，user_account：{user_account}")
+            else:
+                data = result["data"]
+                if data is None:
+                    log.error(f"【抖音-查询直播状态-{self.name}】请求返回数据为空，user_account：{user_account}")
+                    return
+                room_datas = data.get('data')
+                if room_datas is None or len(room_datas) == 0:
+                    log.error(f"【抖音-查询直播状态-{self.name}】【{user_account}】未开通直播间，停止检测")
+                    self.douyin_id_list.remove(user_account)
+                    return
+                try:
+                    room_data = room_datas[0]
+                    room_status = data.get('room_status')
+                    nickname = data.get('user').get('nickname')
+                except AttributeError:
+                    log.error(f"【抖音-查询直播状态-{self.name}】dict取值错误，user_account：{user_account}")
+                    return
+
+                if self.living_status_dict.get(user_account, None) is None:
+                    self.living_status_dict[user_account] = room_status
+                    log.info(f"【抖音-查询直播状态-{self.name}】【{nickname}】初始化")
+                    return
+
+                if self.living_status_dict.get(user_account, None) != room_status:
+                    self.living_status_dict[user_account] = room_status
+
+                    if room_status == 0:
+                        room_title = room_data["title"]
+                        room_cover_url = room_data["cover"]["url_list"][0]
+                        jump_url = f'https://live.douyin.com/{user_account}'
+
+                        log.info(f"【抖音-查询直播状态-{self.name}】【{nickname}】开播了，准备推送：{room_title}")
+                        self.push_for_douyin_live(nickname, jump_url, room_title, room_cover_url)
+
     @staticmethod
     def get_headers():
         return {
@@ -195,13 +292,13 @@ class QueryDouyin(QueryTask):
         content = content[:100] + (content[100:] and "...")
         super().push(title, content, video_url, pic_url, extend_data={'dynamic_raw_data': dynamic_raw_data})
 
-    def push_for_douyin_live(self, nickname=None, room_stream_url=None, room_title=None, room_cover_url=None):
+    def push_for_douyin_live(self, nickname=None, jump_url=None, room_title=None, room_cover_url=None):
         """
         抖音直播提醒推送
         :param nickname: 作者名
-        :param room_stream_url: 推流地址
+        :param jump_url: 跳转地址
         :param room_title: 直播间标题
         :param room_cover_url: 直播间封面
         """
         title = f"【抖音】【{nickname}】开播了"
-        super().push(title, room_title, room_stream_url, room_cover_url)
+        super().push(title, room_title, jump_url, room_cover_url)
