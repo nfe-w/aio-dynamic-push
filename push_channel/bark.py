24e7bc64e8c4d5d938429a5014314a5df53ef13d
nfe-w <nfe-w@outlook.com>
2025-03-06 19:37:37 +0800

    feat(push-bark): 推送支持分组 #42

--- push_channel/bark.py
+++ push_channel/bark.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from common import util
 from common.logger import log
 from . import PushChannel
@@ -11,7 +13,18 @@ class Bark(PushChannel):
             log.error(f"【推送_{self.name}】配置不完整，推送功能将无法正常使用")
 
     def push(self, title, content, jump_url=None, pic_url=None, extend_data=None):
-        push_url = f"https://api.day.app/{self.key}/{title}/{content}?url={jump_url}"
+        push_url = f"https://api.day.app/{self.key}/{title}/{content}"
+
+        params = {}
+        if jump_url:
+            params["url"] = jump_url
+
+        if extend_data:
+            query_task_config = extend_data.get("query_task_config")
+            if query_task_config and "name" in query_task_config:
+                params["group"] = query_task_config["name"]
+
+        push_url = f"{push_url}?{urlencode(params)}" if params else push_url
         response = util.requests_post(push_url, self.name)
         push_result = "成功" if util.check_response_is_ok(response) else "失败"
         log.info(f"【推送_{self.name}】{push_result}")
