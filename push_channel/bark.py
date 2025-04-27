e4b0569220db3ed1105ee94027c41c80c9d7fc82
nfe-w <nfe-w@outlook.com>
2025-04-27 19:26:13 +0800

    feat(push-bark): 支持配置自定义服务器地址 #45

--- push_channel/bark.py
+++ push_channel/bark.py
@@ -8,12 +8,13 @@ from . import PushChannel
 class Bark(PushChannel):
     def __init__(self, config):
         super().__init__(config)
+        self.server_url = str(config.get("server_url", "https://api.day.app"))
         self.key = str(config.get("key", ""))
         if self.key == "":
             log.error(f"【推送_{self.name}】配置不完整，推送功能将无法正常使用")
 
     def push(self, title, content, jump_url=None, pic_url=None, extend_data=None):
-        push_url = f"https://api.day.app/{self.key}/{title}/{content}"
+        push_url = f"{self.server_url}/{self.key}/{title}/{content}"
 
         params = {}
         if jump_url:
